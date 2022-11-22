ffb15e39bf57fe7ee44974953cbd33e7f4a081ff
songzhichao1225 <1051491552@qq.com>
2022-11-22 16:10:24 +0800

    没保存

--- src/pc/nowJoin/now.js
+++ src/pc/nowJoin/now.js
@@ -6,12 +6,8 @@ class perfect2 extends React.Component {
 
 
   render(){
-<<<<<<< HEAD
 
     console.log('ktt') 
-=======
-    console.log(211413)
->>>>>>> 9dc9b4bd2c2447ec5dae69fa653a73eed71c7bde
     return(
         <div>
 
