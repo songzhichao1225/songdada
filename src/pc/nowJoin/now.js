cfe2b725dacd739fa43e503e521b4ceb22217cf3
songzhichao1225 <1051491552@qq.com>
2022-11-22 11:25:38 +0800

    我新增的文件

---
+++ src/pc/nowJoin/now.js
@@ -0,0 +1,23 @@
+
+
+
+class perfect2 extends React.Component {
+
+
+
+  render(){
+    return(
+        <div>
+
+            aseddfedrg
+        </div>
+
+    )
+  }
+    
+  
+
+
+}
+
+export default perfect2
